35a6999c7bcfc1c911bd06e55ac46c975ffc562a
Edgar Robles <54951151+ed-robles@users.noreply.github.com>
2025-10-07 23:21:36 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function HomePage() {
   return (
     <main className="simple-page">
       <div className="simple-page__inner">
-        <p className="font-bold tracking-tight">
-        Robles Creative Company is a Milwaukee-based creative studio specializing in photography, video production, and web development. We help brands and individuals make a lasting impact in the digital space — combining compelling visuals with clean, modern online experiences that stand out and drive results.
+        <p>
+        <span className="font-bold tracking-tight">Robles Creative Company</span> is a Milwaukee-based creative studio specializing in photography, video production, and web development. We help brands and individuals make a lasting impact in the digital space — combining compelling visuals with clean, modern online experiences that stand out and drive results.
         </p>
       </div>
     </main>
