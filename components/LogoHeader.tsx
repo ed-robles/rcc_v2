a9150e89f740018937544e9996cf633c62250000
Edgar Robles <54951151+ed-robles@users.noreply.github.com>
2025-10-23 10:18:44 -0500

    Make header logo link to home

--- components/LogoHeader.tsx
+++ components/LogoHeader.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
+
 const LogoHeader = () => {
   return (
-          <div className="site-header">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/rcc_planet_black.png"
-            alt="RCC logo"
-            style={{ width: 180, height: 101 }}
-          />
-        </div>
-  )
-}
+    <div className="site-header">
+      <Link href="/">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/rcc_planet_black.png"
+          alt="RCC logo"
+          style={{ width: 180, height: 101 }}
+        />
+      </Link>
+    </div>
+  );
+};
 
-export default LogoHeader
+export default LogoHeader;
